135f55692986ac5cd5793955dccfb887e81eb8ef
loffjh <loffjh@gmail.com>
2020-11-08 15:20:08 -0300

    initial configuration

--- sort/test.sh
+++ sort/test.sh
@@ -4,15 +4,15 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo "\n${RED}Unity tests + Address Sanitizer${NC}\n"
-./test_identifier.out
+./test_sort.out
 
 echo "\n${RED}CPPcheck${NC}\n"
-cppcheck --enable=all src/identifier.c 
+cppcheck --enable=all src/sort.c 
 
 
 echo "\n${RED}Valgrind${NC}\n"
-valgrind --leak-check=full --show-leak-kinds=all ./test_identifier.out
+valgrind --leak-check=full --show-leak-kinds=all ./test_sort.out
 
 
 echo "\n${RED}Gcov check${NC}\n"
-gcov -b identifier.gcno
+gcov -b sort.gcno
