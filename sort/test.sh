a8275f6157fa33d62d3ded6dfb490fea4e6253d7
renatobhf <renatobhf@gmail.com.br>
2020-11-16 12:46:35 -0300

    changes

--- sort/test.sh
+++ sort/test.sh
@@ -13,6 +13,8 @@ cppcheck --enable=all src/sort.c
 echo "\n${RED}Valgrind${NC}\n"
 valgrind --leak-check=full --show-leak-kinds=all ./test_sort.out
 
+echo "\n${RED}Sanitizer${NC}\n"
+./test_sort_sanitize.out
 
 echo "\n${RED}Gcov check${NC}\n"
 gcov -b sort.gcno
